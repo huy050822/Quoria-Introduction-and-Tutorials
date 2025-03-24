6c3da1a0d51d4955cd8b5d81e312bb707e39afa4
Quang Huy <huy.tnq@gmail.com>
2025-03-24 20:28:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Welcome to the introduction and tutorial page for Quoria.")
 
 st.header("About Quoria")
 st.write("QUORIA is an intelligent chatbot that helps users determine the value of a used motorcycle based on real-world data. Using a Linear Regression model and data sourced from Aiven Cloud, the bot can analyze key factors such as brand, model, year of manufacture, mileage, and condition to provide the most reasonable price estimate. Since this is a project developed by students, we would greatly appreciate any feedback if you have the opportunity to try it out!")
-st.image("https://via.placeholder.com/600x300", caption="Quoria Interface")
+st.image("https://raw.githubusercontent.com/huy050822/Quoria-Introduction-and-Tutorials/refs/heads/main/Screenshot%202025-03-24%20202714.png", caption="Quoria Interface")
 
 st.header("How to Use Quoria")
 st.write(
